222567f7ae1a67a227ea7ff22f18accc63d3f9cf
JohnKevinDevs <johnkevin.devs@gmail.com>
2026-03-22 16:06:34 -0300

    feat: update structure components

--- react-todo-list/src/components/ListaTarefa/ListaTarefa.jsx
+++
@@ -1,9 +0,0 @@
-import styles from "./ListaTarefa.module.css";
-
-export function ListaTarefa() {
-  return (
-    <div className={styles.container}>
-      <p>ListaTarefa</p>
-    </div>
-  );
-}
---
+++ react-todo-list/src/components/ListaTarefas/ListaTarefas.jsx
@@ -0,0 +1,9 @@
+import styles from "./ListaTarefas.module.css";
+
+export function ListaTarefas() {
+  return (
+    <div className={styles.container}>
+      <p>ListaTarefas</p>
+    </div>
+  );
+}
