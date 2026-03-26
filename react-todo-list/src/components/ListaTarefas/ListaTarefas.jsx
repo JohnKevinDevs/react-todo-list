445dc021f80b101c2fb5cfd0da0471b158aa49eb
JohnKevinDevs <johnkevin.devs@gmail.com>
2026-03-25 22:24:53 -0300

    feat: add listatarefas.jsx final

--- react-todo-list/src/components/ListaTarefas/ListaTarefas.jsx
+++ react-todo-list/src/components/ListaTarefas/ListaTarefas.jsx
@@ -1,9 +1,25 @@
+import { ItemTarefa } from "../ItemTarefa/ItemTarefa";
 import styles from "./ListaTarefas.module.css";
 
-export function ListaTarefas() {
+export function ListaTarefas({
+  tarefas,
+  onAlternarConclusao,
+  onExcluirTarefa,
+}) {
   return (
-    <div className={styles.container}>
-      <p>ListaTarefas</p>
-    </div>
+    <section className={styles.lista}>
+      {tarefas.length === 0 ? (
+        <p className={styles.mensagem}>Nenhuma tarefa cadastrada.</p>
+      ) : (
+        tarefas.map((tarefa) => (
+          <ItemTarefa
+            key={tarefa.id}
+            tarefa={tarefa}
+            onAlternarConclusao={onAlternarConclusao}
+            onExcluirTarefa={onExcluirTarefa}
+          />
+        ))
+      )}
+    </section>
   );
 }
