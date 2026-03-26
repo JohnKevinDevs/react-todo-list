3c06782a3b5b8aca7b3c88f15da554464a6ff677
JohnKevinDevs <johnkevin.devs@gmail.com>
2026-03-25 22:26:04 -0300

    feat: add itemtarefa.jsx final

--- react-todo-list/src/components/ItemTarefa/ItemTarefa.jsx
+++ react-todo-list/src/components/ItemTarefa/ItemTarefa.jsx
@@ -1,9 +1,34 @@
 import styles from "./ItemTarefa.module.css";
 
-export function ItemTarefa() {
+export function ItemTarefa({
+  tarefa,
+  onAlternarConclusao,
+  onExcluirTarefa,
+}) {
   return (
-    <div className={styles.container}>
-      <p>ItemTarefa</p>
-    </div>
+    <article className={styles.item}>
+      <label className={styles.conteudo}>
+        <input
+          type="checkbox"
+          checked={tarefa.concluida}
+          onChange={() => onAlternarConclusao(tarefa.id)}
+        />
+        <span
+          className={
+            tarefa.concluida ? styles.textoConcluido : styles.textoPendente
+          }
+        >
+          {tarefa.titulo}
+        </span>
+      </label>
+
+      <button
+        className={styles.botaoExcluir}
+        type="button"
+        onClick={() => onExcluirTarefa(tarefa.id)}
+      >
+        Excluir
+      </button>
+    </article>
   );
 }
