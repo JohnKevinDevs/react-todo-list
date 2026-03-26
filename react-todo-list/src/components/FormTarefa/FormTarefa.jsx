30d311ce620a33b0f46b5007a0f71fc1a842b7e2
JohnKevinDevs <johnkevin.devs@gmail.com>
2026-03-25 22:23:47 -0300

    feat: add formtarefa.jsx final

--- react-todo-list/src/components/FormTarefa/FormTarefa.jsx
+++ react-todo-list/src/components/FormTarefa/FormTarefa.jsx
@@ -1,9 +1,33 @@
+import { useState } from "react";
 import styles from "./FormTarefa.module.css";
 
-export function FormTarefa() {
+export function FormTarefa({ onAdicionarTarefa }) {
+  const [titulo, setTitulo] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    if (titulo.trim() === "") {
+      return;
+    }
+
+    onAdicionarTarefa(titulo);
+    setTitulo("");
+  }
+
   return (
-    <div className={styles.container}>
-      <p>FormTarefa</p>
-    </div>
+    <form className={styles.form} onSubmit={handleSubmit}>
+      <input
+        className={styles.input}
+        type="text"
+        placeholder="Digite uma tarefa"
+        value={titulo}
+        onChange={(event) => setTitulo(event.target.value)}
+      />
+
+      <button className={styles.botao} type="submit">
+        Adicionar
+      </button>
+    </form>
   );
 }
